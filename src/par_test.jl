c64c5feb50193f4ca955c3de7993da763b7cfd65
martinzh <sstereo.vision@gmail.com>
2016-03-28 15:40:41 -0600

    clean and write to file

--- src/par_test.jl
+++ src/par_test.jl
@@ -1,6 +1,9 @@
 @everywhere using DistributedArrays
 
-raw_data = readcsv("/home/martin/Documents/datos_ecobici/EcobiciDF/2010.csv") # cubo
+# raw_data = readcsv("/home/martin/Documents/datos_ecobici/EcobiciDF/2010.csv") # cubo
+raw_data = readcsv("/home/martin/datos_ecobici/EcobiciDF/2010.csv") # comadreja
+
+file = open("filt_2010.csv", "w")
 
 # Encuentra viajes con status "A"
 trav_A = find( x -> x == "A  ", raw_data[:,7])
@@ -10,11 +13,16 @@ data = raw_data[trav_A,:]
 
 # Distribuye datos en procesadores
 Ddata = distribute(data)
-println(size(Ddata))
+#println(size(Ddata))
 println("pass dist")
 
-for i in 1:300
+for i in 1:10000
+#for i in 1:size(data,1)
     st_time = DateTime(Ddata[i,3], "y-m-d H:M:S")
     end_time = DateTime(Ddata[i,5], "y-m-d H:M:S")
-    println("dur:\t", Dates.Minute(end_time - st_time), "\tsame:\t", Ddata[i,4] == Ddata[i,6] )
+    #println("dur:\t", Dates.Minute(end_time - st_time), "\tsame:\t", Ddata[i,4] == Ddata[i,6] )
+    println(file, Ddata[i,4],",",Ddata[i,6],",",Int(Dates.Minute(end_time - st_time)),",",Dates.dayofweek(st_time),",",Dates.month(st_time))
 end
+
+close(file)
+println("done")
