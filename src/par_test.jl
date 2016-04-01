3db182c53069e2185053028567716327fddf39e9
Martin Zumaya <sstereo.vision@gmail.com>
2016-04-01 11:47:39 -0600

    added hour of start

--- src/par_test.jl
+++ src/par_test.jl
@@ -17,13 +17,13 @@ data = raw_data[trav_A,:]
 Ddata = distribute(data)
 println("pass dist")
 
-println(file, "id_start,id_end,month,day,mins")
+println(file, "id_start,id_end,month,day,hour,mins")
 
 # for i in 1:10000
 for i in 1:size(data,1)
     st_time = DateTime(Ddata[i,3], "y-m-d H:M:S")
     end_time = DateTime(Ddata[i,5], "y-m-d H:M:S")
-    println(file, Ddata[i,4],",",Ddata[i,6],",",Dates.month(st_time),",",Dates.dayofweek(st_time),",",Dates.value(Dates.Minute(end_time - st_time)))
+    println(file, Ddata[i,4], ",", Ddata[i,6], ",", Dates.month(st_time), ",", Dates.dayofweek(st_time), ",",Dates.value(Dates.Hour(st_time)), ",", Dates.value(Dates.Minute(end_time - st_time)))
 end
 
 close(file)
