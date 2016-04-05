a51a912f193221f95dc10dab078f3502dd6931f7
Martin Zumaya <sstereo.vision@gmail.com>
2016-04-05 12:06:32 -0500

    typo

--- src/par_test_last.jl
+++ src/par_test_last.jl
@@ -21,8 +21,8 @@ println(file, "id_start,id_end,month,day,hour,mins")
 
 # for i in 1:10000
 for i in 1:size(raw_data,1)
-    st_time = DateTime(Ddata[i,5]*" "*DateTime(Ddata[i,6], "y-m-d H:M:S")
-    end_time = DateTime(Ddata[i,8]*" "*DateTime(Ddata[i,9], "y-m-d H:M:S")
+    st_time  = DateTime(Ddata[i,5]*" "*DateTime(Ddata[i,6]), "y-m-d H:M:S")
+    end_time = DateTime(Ddata[i,8]*" "*DateTime(Ddata[i,9]), "y-m-d H:M:S")
     println(file, Ddata[i,4], ",", Ddata[i,7], ",", Dates.month(st_time), ",", Dates.dayofweek(st_time), ",",Dates.value(Dates.Hour(st_time)), ",", Dates.value(Dates.Minute(end_time - st_time)))
 end
 
